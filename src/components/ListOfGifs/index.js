d794607b8c70dc711a83ffd7eecb13c9226e3551
huiter rodrigues <61643960+huiterjrv@users.noreply.github.com>
2020-10-02 16:13:38 +0200

    mejora de rendimiento

--- src/components/ListOfGifs/index.js
+++ src/components/ListOfGifs/index.js
@@ -5,12 +5,13 @@ import './styles.css'
 export default function ListOfGifs ({gifs}) {
   return <div className='ListOfGifs'>
     {
-      gifs.map(({id, title, url}) =>
+      gifs.map(({id, title, url , ...ObjNewDeprueva}) =>
         <Gif
           id={id}
           key={id}
           title={title}
           url={url}
+          objInesesario={ObjNewDeprueva}
         />
       )
     }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,30 +1,22 @@
-import React, {useState} from "react"
+import React, {useCallback} from "react"
 import { useLocation } from "wouter"
 import ListOfGifs from 'components/ListOfGifs'
 import {useGifs} from 'hooks/useGifs'
 import TrendingSearches from 'components/TrendingSearches'
+import SearchForm from "components/SearchFrome"
 
 export default function Home() {
-  const [keyword, setKeyword] = useState('')
   const [path, pushLocation] = useLocation()
   const {loading, gifs} = useGifs()
 
-  const handleSubmit = evt => {
-    evt.preventDefault()
+  const handleSubmit = useCallback( ({keyword}) => {
     // navegar a otra ruta
     pushLocation(`/search/${keyword}`)
-  }
-
-  const handleChange = evt => {
-    setKeyword(evt.target.value)
-  }
+  },[pushLocation])
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <button>Buscar</button>
-        <input placeholder="Search a gif here..." onChange={handleChange} type='text' value={keyword} />
-      </form>
+      <SearchForm onSubmit={handleSubmit} />
       <div className="App-main">
         <div className="App-results">
           <h3 className="App-title">Última búsqueda</h3>
